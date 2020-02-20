d6fdd7241c018b3d72bae6ba0d39766d7136ca2c
gracejihaepark <grace.jihae.park@gmail.com>
2020-02-20 17:22:39 -0500

    data cleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,21 +9,36 @@ df = df.drop(columns='Unnamed: 0')
 df.head(2)
 
 
-df = df.drop(columns=['creation_tsz', 'ending_tsz', 'original_creation_tsz', 'last_modified_tsz', 'sku', 'state_tsz', 'shipping_template_id', 'item_weight', 'item_weight_unit', 'item_length', 'item_width', 'item_height', 'item_dimensions_unit', 'non_taxable', 'is_customizable', 'is_digital', 'file_data', 'should_auto_renew', 'has_variations', 'used_manufacturer', 'is_vintage'])
+df = df.drop(columns=['creation_tsz', 'ending_tsz', 'original_creation_tsz', 'last_modified_tsz', 'sku', 'state_tsz', 'shipping_template_id', 'item_weight', 'item_weight_unit', 'item_length', 'item_width', 'item_height', 'item_dimensions_unit', 'non_taxable', 'is_customizable', 'is_digital', 'file_data', 'should_auto_renew', 'has_variations', 'used_manufacturer', 'is_vintage', 'featured_rank', 'is_private', 'who_made', 'style'])
 
-len(df.taxonomy_id.unique())
 
+df.to_csv('listings.csv')
+
+
+df.isnull().sum()
+df = df.dropna(subset = ['user_id'])
 df.to_csv('listings.csv')
 
 df = pd.read_csv('listings.csv')
 df = df.drop(columns='Unnamed: 0')
-
-df.isnull().sum()
-
-df = df.dropna(subset = ['user_id'])
-
-df.isnull().sum()
-
-df.to_csv('listings.csv')
-
 df
+
+
+df.isnull().sum()
+
+df.taxonomy_id.value_counts()
+
+len(df.taxonomy_id.unique())
+
+dftax = (df[df.groupby('taxonomy_path')['taxonomy_path'].transform('size') >= 200])
+dftax
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.figure(figsize=(15,7))
+plt.xticks(rotation=90)
+ax = sns.countplot(x='taxonomy_path', data=dftax)
+
+
+
+df[df['taxonomy_id'] == 1.0]
